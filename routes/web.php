6a95c87c0b09885ab3032750ee2823a836a49aea
Madhurya Dutta <97863234+madhuryadutta@users.noreply.github.com>
2024-02-14 01:08:07 +0530

    temp save

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,4 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('ckeditor/image_upload', 'EditorController@upload')->name('upload');
